bfd7506bfed8749584cc2d0a26b29c1311cef2c9
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-05-16 18:45:08 +0900

    いらない行削除

--- src/App.ts
+++ src/App.ts
@@ -8,16 +8,3 @@ export class App {
     bootCac();
   }
 }
-
-// #11 これ以下を別ファイルに分離したい
-
-// const debug = () => {
-//   const table = tableForDebug;
-//   read().forEach((k, v) => {
-//     const id = k;
-//     const {taskKind, content, deadline, done, deleted} = v;
-//     const taskShaped = [id, convertBool(done), taskKind, content, deadline, deleted];
-//     return table.push(taskShaped);
-//   })
-//   console.log(table.toString());
-// }
