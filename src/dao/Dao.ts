0c6443de45e728cfe716d8519af5689a25511af4
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-05-26 22:11:52 +0900

    README を修正

--- src/dao/Dao.ts
+++ src/dao/Dao.ts
@@ -4,6 +4,19 @@ import { TodoProps } from '../models/Todo';
 
 export const path = 'todo.json';
 
+export const sureJSONExist = () => {
+  try {
+    fs.statSync(path);
+
+    return;
+  } catch (e) {
+    if (e.code === 'ENOENT') {
+      fs.writeFileSync(path, '');
+      console.log('todo.json generated.');
+    }
+  }
+};
+
 export const updateFile = (todoMap: Map<number, TodoProps>): void => {
   fs.writeFileSync(path, JSON.stringify(Array.from(todoMap)));
 };
