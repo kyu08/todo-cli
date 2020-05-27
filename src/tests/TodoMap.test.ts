7900e76c5e8702871491f071f6b969ce911914a1
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-05-27 20:11:51 +0900

    テストケースを1つ追加

--- src/tests/TodoMap.test.ts
+++ src/tests/TodoMap.test.ts
@@ -1,9 +1,15 @@
 import { guardIncorrectId } from '../models/TodoMap';
 
-describe('test', (): void => {
-  test('hoge', (): void => {
+describe('guardIncorrectId', (): void => {
+  test('NaN', (): void => {
     const id = Number(NaN);
     const res = guardIncorrectId(id);
     expect(res).toBe(true);
+  });
+
+  test('correctId', (): void => {
+    const id = 1;
+    const res = guardIncorrectId(id);
+    expect(res).toBe(false);
   });
 });
