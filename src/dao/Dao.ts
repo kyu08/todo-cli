531a70266dd2d7f073568a6f45c52b6fbb1f57ad
Tatsuya Kyushima <kyu08@users.noreply.github.com>
2020-05-28 15:59:56 +0900

    fix todoコマンドを実行したパスにtodo.jsonが作成されてしまう問題を修正

--- src/dao/Dao.ts
+++ src/dao/Dao.ts
@@ -2,25 +2,40 @@ import fs from 'fs';
 // eslint-disable-next-line import/no-cycle
 import { TodoProps } from '../models/Todo';
 
-export const path = 'todo.json';
+const homeDirectoryPath = process
+  .cwd()
+  .split('/')
+  .slice(0, 3)
+  .join('/');
+
+const filePath = `${homeDirectoryPath}/.great-todo-cli/todo.json`;
+const appRootPath = `${homeDirectoryPath}/.great-todo-cli`;
+
+export const sureDirectoryExist = (): void => {
+  if (!fs.existsSync(appRootPath)) {
+    fs.mkdirSync(appRootPath);
+    console.log(`App Root Directory generated. -> ${appRootPath} `);
+    console.log(`todo.json is stored in -> ${appRootPath}.`);
+  }
+};
 
 export const sureJSONExist = (): void => {
   try {
-    fs.statSync(path);
+    fs.statSync(filePath);
 
     return;
   } catch (e) {
     if (e.code === 'ENOENT') {
-      fs.writeFileSync(path, '');
+      fs.writeFileSync(filePath, '');
       console.log('todo.json generated.');
     }
   }
 };
 
 export const updateFile = (todoMap: Map<number, TodoProps>): void => {
-  fs.writeFileSync(path, JSON.stringify(Array.from(todoMap)));
+  fs.writeFileSync(filePath, JSON.stringify(Array.from(todoMap)));
 };
 
 export const loadFile = (): string => {
-  return fs.readFileSync(path, 'utf-8');
+  return fs.readFileSync(filePath, 'utf-8');
 };
